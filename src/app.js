63794dea37bba2bf72e2f40fba5c217bedfd9c9c
Shadrach-stack <obaahshadrach416@gmail.com>
2026-04-17 07:52:55 +0100

    test route only

--- src/app.js
+++ src/app.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-
-const profileRoutes = require("./routes/profile.routes");
-
-const app = express();
-
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-
-// base route
-app.use("/api", profileRoutes);
-
-module.exports = app;
+app.get("/test", (req, res) => {
+  res.json({ ok: true });
+});
